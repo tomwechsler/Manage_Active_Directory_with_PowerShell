449e2e926e7a0c65a62fc6d160ed3ec974809cf4
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2020-12-21 17:59:42 +0100

    Add files via upload

---
+++ 01_Erste_Schritte/02_Erste_Befehle.ps1
@@ -0,0 +1,23 @@
+﻿Clear-Host
+
+Set-Location c:\
+
+dir
+
+cls
+
+Get-Alias cls
+
+Get-Item c:\Windows\
+
+Get-Item c:\Windows\*
+
+gi c:\Windows\
+
+Get-Command
+
+gcm
+
+gal
+
+Set-Alias -Name geti -Value Get-Item
---
+++ 01_Erste_Schritte/03_Hilfe.ps1
@@ -0,0 +1,29 @@
+﻿Clear-Host
+
+Set-Location c:\
+
+Get-Help Get-ChildItem
+
+Get-ChildItem -?
+
+Get-Help Get-Help
+
+Get-Help -Category Cmdlet
+
+man Get-ChildItem
+
+help Get-ChildItem
+
+Get-Help Get-ChildItem -Detailed
+
+Get-Help Get-ChildItem -Parameter *
+
+Get-Help Get-ChildItem -Full
+
+Get-Help Get-ChildItem -Examples
+
+Get-Help about_*
+
+Get-Help about_command_syntax
+
+Get-Help registry
